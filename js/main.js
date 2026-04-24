c61f2709a0420435b4a2d4f57378f5e1bdbd09c1
pfischer-dev <paulofischer.dev>
2026-04-24 11:52:36 -0300

    ajustes borda hub central

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     const scrollAtual = window.scrollY;
 
-    if(scrollAtual > scrollInicial && scrollAtual > 30) {
+    if(scrollAtual > scrollInicial && scrollAtual > 100) {
         header.classList.add('hidden');
     }
     else {
